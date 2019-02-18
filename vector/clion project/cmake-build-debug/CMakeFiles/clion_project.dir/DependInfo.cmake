38680a33e7b355026bb2ac32a8c676e3732e8a9e
songdingchuan <songdc@litsoft.com.cn>
2019-02-18 09:09:54 +0800

    添加clioon工程文件

---
+++ vector/clion project/cmake-build-debug/CMakeFiles/clion_project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/c-utils/vector/main.c" "E:/c-utils/vector/clion project/cmake-build-debug/CMakeFiles/clion_project.dir/E_/c-utils/vector/main.c.obj"
+  "E:/c-utils/vector/sort_find.c" "E:/c-utils/vector/clion project/cmake-build-debug/CMakeFiles/clion_project.dir/E_/c-utils/vector/sort_find.c.obj"
+  "E:/c-utils/vector/vector.c" "E:/c-utils/vector/clion project/cmake-build-debug/CMakeFiles/clion_project.dir/E_/c-utils/vector/vector.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vector/clion project/cmake-build-debug/CMakeFiles/clion_project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_project.dir/E_/c-utils/vector/main.c.obj"
+  "CMakeFiles/clion_project.dir/E_/c-utils/vector/sort_find.c.obj"
+  "CMakeFiles/clion_project.dir/E_/c-utils/vector/vector.c.obj"
+  "clion_project.pdb"
+  "clion_project.exe"
+  "clion_project.exe.manifest"
+  "libclion_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clion_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
